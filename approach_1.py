f12c261774607978bf4f8705f0e26019e5c9ee28
noufida <noufidap@gmail.com>
2022-08-28 11:28:42 +0530

    4th

--- approach_1.py
+++ approach_1.py
@@ -1,13 +1,14 @@
 #APPROACH 1
 #user enters the ctc
-#loop through columnA of salary structure and checks whether there is such a column which is the most independent one. 
+#loop through columnA of salary structure and checks whether there is a column corresponding to ctc which is the most independent one. 
 #if found, change the value of column to the entered ctc
-#get the expressions for each cells using .value
+#get the expressions for each cells using .value 
 #update every cells whose expression contains the independent value
 #update every cells which is depending the updated cells
 
 #CHALLENGE FACED:the expression was getting in a string data type.so,confused about converting that into a regular expression.
-                     #tried splitting them to each components. but felt like complicated
+#tried splitting them to each components(like, first split the string with '+' symbol, and add the resulting string. then split each with '-', and find difference, then with '*' and '/' and so on)
+#it felt complicated
 import openpyxl
  
 wb = openpyxl.load_workbook("test.xlsx",data_only=True)
--- approach_2.py
+++ approach_2.py
@@ -20,7 +20,8 @@ def basic_salary():
             ws['C'+str(x)].value=(ws['B'+str(a)].value*0.5)/12
             wb.save("test.xlsx")
             basic_salary=ws['C'+str(x)].value
-            print(basic_salary)
+            print("monthly basic salary =",basic_salary)
+            print("annual basic salary =", ws['B'+str(x)].value)
 
 def hra():
     for x in range(9,20):
@@ -33,22 +34,33 @@ def hra():
             print("annual hra=",annual_hra)
             print("monthly hra=",monthly_hra)
 
+def employer_esi():
+    for x in range(9,20):
+        if ws['A'+str(x)].value == 'Employer_ESI':
+            if ws['B'+str(x+1)].value<252001:
+                ws['B'+str(x)].value=ws['B'+str(x+1)].value*0.0325
+                ws['C'+str(x)].value=(ws['B'+str(x)].value)/12
+            else:
+                ws['B'+str(x)].value=0
+                ws['C'+str(x)].value=(ws['B'+str(x)].value)/12
+            wb.save("test.xlsx")
+            monthly_ESI=ws['C'+str(x)].value
+            annual_ESI=ws['B'+str(x)].value
+            print("annual ESI=",annual_ESI)
+            print("monthly ESI=",monthly_ESI)
+
 
 base_salary=(int(input("enter ctc")))
 for x in range(9,20):
     if ws['A'+str(x)].value == 'base_salary':
         ws['B'+str(x)].value=base_salary
         a=x
-print(a,"base")
+        
+# print(a,"base")
 wb.save("test.xlsx")
-# basic_salary()
-# hra()
+
+basic_salary()
+hra()
+employer_esi()
 # base_salary=ws['B14'].value
 
-# for x in range(9,14):
-#     for y in range(1,3):
-#         char = chr(65+y)
-#         temp=char+str(x)
-#         if 'B14' in ws[temp].value:
-#             print('yes')
-        
